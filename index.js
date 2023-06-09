70ca5040a4f7f627ea5200525b039b63b338f892
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-06-09 18:14:07 +0600

    modified all api and added verify jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -16,6 +17,36 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
+const verifyJWT = (req, res, next) => {
+
+
+    const authorization = req.headers.authorization
+
+
+
+    if (!authorization) {
+
+        return res.status(403).send({ error: true, message: 'unauthorization author' })
+
+    }
+
+    const token = authorization.split(' ')[1]
+
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+
+
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorization err' })
+        }
+
+        req.decoded = decoded
+
+        next()
+
+    });
+
+}
 
 
 
@@ -44,7 +75,55 @@ async function run() {
 
 
 
+        // jwt method 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user?.status !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next()
 
+
+        }
+
+
+        const verifyInstructor = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user?.status !== 'instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next()
+
+
+        }
+
+        const verifyStudent = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user?.status !== 'student') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next()
+
+
+        }
+
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '1hr'
+            })
+
+            res.send({ token })
+
+        })
 
 
 
@@ -97,7 +176,8 @@ async function run() {
         })
 
 
-        app.get('/users', async (req, res) => {
+
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray()
             // console.log('get user', result)
             res.send(result)
@@ -105,7 +185,7 @@ async function run() {
 
 
 
-        app.delete('/user/delete/:id', async (req, res) => {
+        app.delete('/user/delete/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query)
@@ -157,7 +237,7 @@ async function run() {
         })
 
 
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -172,14 +252,14 @@ async function run() {
         })
         // student 
 
-        app.post('/select-class', async (req, res) => {
+        app.post('/select-class', verifyJWT, verifyStudent, async (req, res) => {
             const selectedClass = req.body
 
             const result = await selectedClassCollection.insertOne(selectedClass)
             res.send(result)
 
         })
-        app.get('/select-class/:email', async (req, res) => {
+        app.get('/select-class/:email', verifyJWT, verifyStudent, async (req, res) => {
             const email = req.params.email
             const query = { email: email }
             const result = await selectedClassCollection.find(query).toArray()
@@ -187,17 +267,26 @@ async function run() {
         })
 
 
+        app.delete('/select-class/delete/:id', verifyJWT, verifyStudent, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await selectedClassCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
 
         // instructor 
-        app.post('/added-class', async (req, res) => {
+        app.post('/added-class', verifyJWT, verifyInstructor, async (req, res) => {
             const classes = req.body
+            console.log(classes)
             const result = await classesCollection.insertOne(classes)
             res.send(result)
 
         })
 
 
-        app.get('/instructor-classes/:email', async (req, res) => {
+        app.get('/instructor-classes/:email', verifyJWT, verifyInstructor, async (req, res) => {
             const email = req.params.email
             const query = { instructor_email: email }
             console.log(query)
@@ -208,7 +297,7 @@ async function run() {
         })
 
 
-        app.patch('/users/instructor/:id', async (req, res) => {
+        app.patch('/users/instructor/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -222,10 +311,20 @@ async function run() {
 
         })
 
+        app.delete('/instructor-class/delete/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await classesCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
+
 
         // Admin petch method 
 
-        app.patch('/user/admin/:id', async (req, res) => {
+        app.patch('/user/admin/approve/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -241,9 +340,10 @@ async function run() {
 
 
 
-        app.patch('/user/admin/denied/:id', async (req, res) => {
+        app.patch('/user/admin/denied/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const feedback = req.body.feedback
+            console.log(feedback)
             console.log(id, feedback)
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
